85f047191dae8341190fff6c54c56c33f1737543
JLdiesel <1317662314@qq.com>
2022-04-10 11:28:34 +0800

    feat:添加webpack

---
+++ 个人/webpack/splitChunk/webpack.config.js
@@ -0,0 +1,102 @@
+// Generated using webpack-cli https://github.com/webpack/webpack-cli
+
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const WorkboxWebpackPlugin = require("workbox-webpack-plugin");
+const TerserPlugin = require("terser-webpack-plugin");
+const {CleanWebpackPlugin}=require('clean-webpack-plugin')
+const {VueLoaderPlugin}=require('vue-loader')
+const isProduction = process.env.NODE_ENV == "production";
+
+const stylesHandler = MiniCssExtractPlugin.loader;
+
+const config = {
+     entry: [ "./src/index.ts"],
+  output: {
+    path: path.resolve(__dirname, "dist"),
+  },
+  devtool: 'cheap-module-source-map',
+  devServer: {
+    open: true,
+    host: "localhost",
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      template: "index.html",
+    }),
+    new VueLoaderPlugin(),
+    new MiniCssExtractPlugin(),
+    // Add your plugins here
+    // Learn more about plugins from https://webpack.js.org/configuration/plugins/
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.(ts|tsx)$/i,
+        use: ["ts-loader","babel-loader"],
+        exclude: ["/node_modules/"],
+      },{
+        test: /\.vue$/,
+        use: ["vue-loader"],
+      },
+      {
+        test: /\.less$/i,
+        use: [stylesHandler, "css-loader", "postcss-loader", "less-loader"],
+      },
+      {
+        test: /\.css$/i,
+        use: [stylesHandler, "css-loader", "postcss-loader"],
+      },
+      {
+        test: /\.(eot|svg|ttf|woff|woff2|png|jpg|gif)$/i,
+        type: "asset",
+      },
+
+      // Add your rules for custom modules here
+      // Learn more about loaders from https://webpack.js.org/loaders/
+    ],
+  },
+  resolve: {
+    extensions: [".tsx", ".ts", ".js"],
+  },
+    optimization: {
+      minimize: true,
+      minimizer: [new TerserPlugin({extractComments: true})],
+      chunkIds: 'named',
+      usedExports: true,
+      splitChunks: {
+      chunks: 'all',
+      minSize: 20000,
+      minRemainingSize: 0,
+      minChunks: 1,
+      maxAsyncRequests: 30,
+      maxInitialRequests: 30,
+      enforceSizeThreshold: 50000,
+      cacheGroups: {
+        defaultVendors: {
+          test: /[\\/]node_modules[\\/]/,
+          priority: -10,
+          reuseExistingChunk: true,
+        },
+        default: {
+          minChunks: 2,
+          priority: -20,
+          reuseExistingChunk: true,
+        },
+      },
+    },
+  },
+};
+
+module.exports = () => {
+  if (isProduction) {
+    config.mode = "development";
+
+    config.plugins.push(new WorkboxWebpackPlugin.GenerateSW());
+  } else {
+    config.mode = "development";
+  }
+  return config;
+};
