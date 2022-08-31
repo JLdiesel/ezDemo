d0d4ee2bfa4e4feff7e0373d4d80b4fae14c793e
longya <longya@tsign.cn>
2022-08-31 18:53:04 +0800

    feat: FID

--- 个人/性能监控学习/src/monitor/lib/blankScreen.ts
+++ 个人/性能监控学习/src/monitor/lib/blankScreen.ts
@@ -1,44 +1,41 @@
-import tracker from "../utils/tracker";
-import onload from '../utils/onload'
+import tracker from '../utils/tracker';
+import onload from '../utils/onload';
 export default function () {
-  const wrapperElements=['html','body','#container','.content']
-  let emptyPoints = 0
+  const wrapperElements = ['html', 'body', '#container', '.content'];
+  let emptyPoints = 0;
   function getSelector(element: Element) {
     if (element.id) {
-      return '#'+element.id
+      return '#' + element.id;
     } else if (element.className) {
-      return '.'+element.className.split(' ').filter(Boolean).join('.')
-    }else{
-      return element.nodeName.toLowerCase()
+      return '.' + element.className.split(' ').filter(Boolean).join('.');
+    } else {
+      return element.nodeName.toLowerCase();
     }
   }
-  function isWrapper(element:Element) {
+  function isWrapper(element: Element) {
     let selector = getSelector(element);
     if (~wrapperElements.indexOf(selector)) {
-      console.log(selector);
-      
-      emptyPoints++
+      emptyPoints++;
     }
   }
- 
+
   onload(() => {
-    console.log('load');
-  for (let i = 1; i <= 9; i++) {
-    const xElements = document.elementsFromPoint(window.innerWidth * i / 10, window.innerHeight / 2);
-    const yElements = document.elementsFromPoint(window.innerWidth / 2, window.innerHeight * i / 10);
-    isWrapper(xElements[0])
-    isWrapper(yElements[0])
-  }
-  if (emptyPoints >=15) {
-    let centerElements=document.elementsFromPoint(window.innerWidth / 2, window.innerHeight / 2)
-    tracker.send({
-      kind: 'stability',
-      type: 'blank',
-      emptyPoints,
-      screen: window.screen.width + 'X' + window.screen.height,
-      viewPoint: window.innerWidth + 'X' + window.innerHeight,
-      selector:getSelector(centerElements[0])
-    })
-  }
-  })
+    for (let i = 1; i <= 9; i++) {
+      const xElements = document.elementsFromPoint((window.innerWidth * i) / 10, window.innerHeight / 2);
+      const yElements = document.elementsFromPoint(window.innerWidth / 2, (window.innerHeight * i) / 10);
+      isWrapper(xElements[0]);
+      isWrapper(yElements[0]);
+    }
+    if (emptyPoints >= 15) {
+      let centerElements = document.elementsFromPoint(window.innerWidth / 2, window.innerHeight / 2);
+      tracker.send({
+        kind: 'stability',
+        type: 'blank',
+        emptyPoints,
+        screen: window.screen.width + 'X' + window.screen.height,
+        viewPoint: window.innerWidth + 'X' + window.innerHeight,
+        selector: getSelector(centerElements[0]),
+      });
+    }
+  });
 }
--- 个人/性能监控学习/src/monitor/lib/timing.ts
+++ 个人/性能监控学习/src/monitor/lib/timing.ts
@@ -1,8 +1,10 @@
+import getLastEvent from '../utils/getLastEvent';
+import getSelector from '../utils/getSelector';
 import onload from '../utils/onload';
 import tracker from '../utils/tracker';
 export default function () {
   onload(function () {
-    let FMP, LCP;
+    let FMP: PerformanceEntry, LCP: PerformanceEntry;
     new PerformanceObserver((entryList, observer) => {
       const perfEntries = entryList.getEntries();
       FMP = perfEntries[0];
@@ -12,7 +14,30 @@ export default function () {
       const perfEntries = entryList.getEntries();
       LCP = perfEntries[0];
       observer.disconnect(); //不再观察
-    }).observe({ entryTypes: ['largest-contentful-paint'] }); //观察页面中有意义的元素
+    }).observe({ entryTypes: ['largest-contentful-paint'] }); //观察页面中最大渲染的元素
+
+    new PerformanceObserver((entryList, observer) => {
+      //firstInputDelay FID
+      let lastEvent = getLastEvent();
+      const firstInput = entryList.getEntries()[0];
+      console.log('firstInput', firstInput);
+
+      if (firstInput) {
+        let inputDelay = (firstInput as any).processingStart - firstInput.startTime; //输入到输入响应的时间
+        let duration = firstInput.duration;
+        if (inputDelay > 0 || duration > 0) {
+          tracker.send({
+            kind: 'experience',
+            type: 'firstInputDelay',
+            inputDelay, //延迟时间
+            duration, //处理时间
+            startTime: firstInput.startTime,
+            selector: lastEvent ? getSelector((lastEvent as any).path) : '',
+          });
+        }
+      }
+      observer.disconnect(); //不再观察
+    }).observe({ type: 'first-input', buffered: true }); //第一次交互
     setTimeout(() => {
       console.log(window.performance.getEntriesByType('navigation')[0]);
       const {
@@ -40,6 +65,12 @@ export default function () {
         timeToInteractive: domInteractive - fetchStart, //首次可交互时间
         loadTime: loadEventStart - fetchStart, //完整的加载时间
       });
+      const FP = performance.getEntriesByName('first-paint')[0];
+      const FCP = performance.getEntriesByName('first-contentful-paint')[0];
+      console.log('FP', FP);
+      console.log('FCP', FCP);
+      console.log('FP', FMP);
+      console.log('FP', LCP);
     }, 3000);
   });
 }
