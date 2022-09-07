929f6b684f75cd29379555130a4a1eceb0b7bcd1
longya <longya@tsign.cn>
2022-09-07 19:29:38 +0800

    feat: patchProp

--- 个人/my_vue3/packages/runtime-core/src/renderer.ts
+++ 个人/my_vue3/packages/runtime-core/src/renderer.ts
@@ -1,4 +1,6 @@
-import { ShapeFlags } from "@vue/shared"
+import { patchProp } from "@test/runtime-dom/src/patchProp"
+import { isString, ShapeFlags } from "@vue/shared"
+import { createVnode, Fragment, isSameVnode, Text } from "./vnode"
 
 export function createRenderer(options) {
   const {
@@ -17,9 +19,16 @@ export function createRenderer(options) {
     cloneNode: hostCloneNode,
     patchProp: hostPatchProp
   } = options
+  const normalize = (child) => {
+    if (isString(child)) {
+      return createVnode(Text, null, child)
+    }
+    return child
+  }
   const mountChildren = (container, children) => {
     for (let i = 0; i < children.length; i++) {
-      patch(null, children[i], container)
+      const child = normalize(children[i])
+      patch(null, child, container)
     }
   }
   const mountElement = (vnode, container) => {
@@ -37,15 +46,96 @@ export function createRenderer(options) {
     }
     hostInsert(el, container)
   }
+  const processText = (n1, n2, container) => {
+    if (n1 === null) {
+      hostInsert(n2.el = hostCreateText(n2.children), container)
+    } else {
+      //文本的内容变化了，复用老的节点
+      const el = n2.el = n1.el
+      if (n1.children !== n2.children) {
+        hostSetElementText(el, n2.children)
+      }
+    }
+  }
+  const processFragment = (n1, n2, container) => {
+    let { props, children, shapeFlag } = n2
+    const el = n2.el = container
+    if (props) {
+      for (const key in props) {
+        hostPatchProp(el, key, null, props[key])
+      }
+    }
+    if (shapeFlag & ShapeFlags.TEXT_CHILDREN) {
+      hostSetElementText(el, children)
+    } else if (shapeFlag & ShapeFlags.ARRAY_CHILDREN) {
+      mountChildren(el, children)
+    }
+  }
+  const patchProps = (oldProps, newProps, el) => {
+    for (const key in newProps) { //新的里面有老的没有
+      hostPatchProp(el, key, oldProps[key], newProps[key])
+    }
+    for (const key in oldProps) {
+      if (newProps[key] === null) { //老的有新的没有
+        hostPatchProp(el, key, oldProps[key], null)
+      }
+    }
+  }
+  const patchChildren = (n1, n2, el) => {
+    //比较两个虚拟节点的儿子的差异  el就是当前的父节点
+  }
+  const patchElement = (n1, n2) => { //先复用节点、再比较属性、再比较儿子
+    const el = n2.el = n1.el
+    const oldProps = n1.props || {}
+    const newProps = n2.props || {}
+    patchProps(oldProps, newProps, el);
+    patchChildren(n1, n2, el);
+  }
+  const processElement = (n1, n2, container) => {
+    if (n1 === null) {
+      mountElement(n2, container)
+    } else {
+      //元素比对
+      patchElement(n1, n2)
+    }
+  }
+  /* 
+  如果前后完全没关系，删除老的添加新的
+  老的和新的一样 复用。属性可能不一样，再比对、更新属性
+  比儿子
+  */
+  const unmount = (vnode) => {
+    hostRemove(vnode.el)
+  }
   const patch = (n1, n2, container) => {
     // n2 可能是个文本
     if (n1 === n2) return
-    if (n1 === null) {
-      //初次渲染
-      mountElement(n2, container)
-    } else {
-      //更新流程
+    if (n1 && !isSameVnode(n1, n2)) {
+      //不是同一个节点
+      unmount(n1);  //删除老的
+      n1 = null
     }
+    const { type, shapeFlag } = n2
+
+    switch (type) {
+      case Text:
+        processText(n1, n2, container)
+        break;
+      case Fragment:
+        processFragment(n1, n2, container);
+        break;
+      default:
+        if (shapeFlag & ShapeFlags.ELEMENT) {
+          processElement(n1, n2, container)
+        }
+        break;
+    }
+    // if (n1 === null) {
+    //   //初次渲染
+    //   mountElement(n2, container)
+    // } else {
+    //   //更新流程
+    // }
   }
   const render = (vnode, container) => {
     //如果当前vnode=null 
--- 个人/my_vue3/packages/runtime-core/src/vnode.ts
+++ 个人/my_vue3/packages/runtime-core/src/vnode.ts
@@ -1,7 +1,12 @@
 //type props children
 
 import { isArray, isString, ShapeFlags } from "@vue/shared";
+export const Text = Symbol('Text')
+export const Fragment = Symbol('Fragment')
+export function isSameVnode(n1, n2) { //判断两个虚拟节点是否相同  1、标签名相同 2、key是一样的
+  return (n1.type === n2.type) && (n1.key === n2.key)
 
+}
 //组件,元素,文本
 export function createVnode(type, props, children: any[] | null | string = null) {
   //组合方案  我想只要一个元素中包含的是多个儿子还是一个儿子
