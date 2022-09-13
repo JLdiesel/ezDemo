730b5245a04dd920899455398b482de4b947bdcf
longya <longya@tsign.cn>
2022-09-13 13:54:24 +0800

    feat: 修改event支持数组，冒泡事件首次不触发

--- 个人/my_vue3/packages/runtime-dom/modules/event.ts
+++ 个人/my_vue3/packages/runtime-dom/modules/event.ts
@@ -1,13 +1,25 @@
-function createInvoker(cb: (e) => any) {
-  const invoker = (e) => invoker.value(e)
+type eventFn = Array<(e) => any> | ((e) => any)
+function createInvoker(cb: eventFn) {
+  const invoker = (e) => {
+    if (e.timeStamp < invoker.attached) return
+    if (Array.isArray(invoker.value)) {
+      invoker.value.forEach(fn => {
+        fn(e)
+      });
+    } else {
+      invoker.value(e)
+    }
+  }
   invoker.value = cb
+  invoker.attached = performance.now()
   return invoker
 }
-export function patchEvent(el: any, eventName: string, nextValue: (e) => any) {
+export function patchEvent(el: any, eventName: string, nextValue: eventFn) {
   const invokers = el._vei || (el.vei = {})
   const exits = invokers[eventName]
   if (exits && nextValue) {
     exits.value = nextValue
+    exits.attached = performance.now()
   } else {
     // onClick=click
     const event = eventName.slice(2).toLowerCase();
