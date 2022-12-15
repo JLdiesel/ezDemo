a20cac282d97c073e6f2332257733598207ea5ad
longya <longya@tsign.cn>
2022-12-15 18:25:00 +0800

    feat: 导出reactive

--- 个人/my_vue3/packages/runtime-dom/src/index.ts
+++ 个人/my_vue3/packages/runtime-dom/src/index.ts
@@ -7,3 +7,4 @@ const renderOptions = Object.assign({ patchProp }, nodeOps)
 // createRenderer(renderOptions).render(h('h1', 'hello'), document.getElementById('app'))
 export const render = createRenderer(renderOptions).render
 export * from '@vue/runtime-core'
+export * from '@vue/reactivity'
